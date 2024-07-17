80d634731c19f9da036e1875cf5c66f560bc5598
karen-maria-philip <karenmariaphilip@gmail.com>
2024-07-17 15:40:13 +0300

    add work

---
+++ app/src/main/java/com/example/enwalletdetails/Expenses.kt
@@ -0,0 +1,8 @@
+package com.example.enwalletdetails
+
+data class Expenses(
+    var avator: String,
+    var Salary:String,
+    var date:String,
+    var Money:String
+)
---
+++ app/src/main/java/com/example/enwalletdetails/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.enwalletdetails
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.enwalletdetails.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvExpenses.layoutManager=LinearLayoutManager(this)
+        displayExpenses()
+
+    }
+    fun displayExpenses(){
+        val expenses1 = Expenses("", "Salary", "1st July","KES 42000")
+        val expenses2 = Expenses("", "Salary", "1st July","KES 42000")
+        val expenses3 = Expenses("", "Salary", "1st July","KES 42000")
+        val expenses4 = Expenses("", "Salary", "1st July","KES 42000")
+        val expenses5 = Expenses("", "Salary", "1st July","KES 42000")
+        val expenses6 = Expenses("", "Salary", "1st July","KES 42000")
+        val expenses7 = Expenses("", "Salary", "1st July","KES 42000")
+        val expenses8 = Expenses("", "Salary", "1st July","KES 42000")
+        val expenses9 = Expenses("", "Salary", "1st July","KES 42000")
+
+
+        val expensesList = listOf(expenses1, expenses2, expenses3, expenses4, expenses5, expenses6, expenses7, expenses8,expenses9)
+        val expensesAdapter = ExpensesAdapter(expensesList)
+        binding.rvExpenses.adapter=expensesAdapter
+
+    }
+}
